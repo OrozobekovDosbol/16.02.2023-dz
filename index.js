92d25f900a908679ff655709d2837480cc8ac2e2
Orozobekov Dosbol <orozobekovdosbol@gmail.com>
2023-02-17 00:12:02 +0600

    exercise number 1

--- index.js
+++ index.js
@@ -4,3 +4,14 @@ function myName(name, surname) {
 
 myName("Dosbol", "Orozobekov")
 
+function number(num) {
+  if (num % 2 === 0) {
+    console.log(num + " - четное");
+  }
+  else if (num % 2 === 1) {
+    console.log(num + " - нечетное");
+  }
+}
+
+number(15)
+number(2)
